90da60980a85f48954aaf2313418b1c9536c9c28
manuerios1 <manuelalejandroriosmesa@gmail.comgit config --global user.email manuelalejandroriosmesa@gmail.comgit config --global user.email manuelalejandroriosmesa@gmail.com>
2026-09-23 10:34:45 -0500

    Corregir T minuscula

--- src/pages/Contacto/componentes/FormFile.jsx
+++ src/pages/Contacto/componentes/FormFile.jsx
@@ -1,4 +1,4 @@
-import tubo from "../../../assets/tubo.jpg"
+import tubo from "../../../assets/Tubo.jpg"
 import { useDropzone } from "react-dropzone";
 import { useState } from "react";
 import { useEffect } from "react";
